e304ae418ceba062ce2e1eec6268a472a7094264
Priyadarshinee Ratha <pratyasha.ratha@accenture.com>
2023-05-07 14:56:34 +0530

    folder structure changed

--- WebApp/todoManage/src/app/app.module.ts
+++ WebApp/todoManage/src/app/app.module.ts
@@ -1,31 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
-import { LoginComponent } from './components/login/login.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CustomMaterialModule } from './modules/material.module';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { CoreModule } from './modules/core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatIconModule,
     BrowserAnimationsModule,
-    CustomMaterialModule,
-    MatFormFieldModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
